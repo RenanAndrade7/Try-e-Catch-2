36094bfaf8551e60c1f09709cacaf8abc5df8699
Renan Andrade <renan.automation@gmail.com>
2021-04-28 17:31:46 -0300

    Novo Catch inserido

---
+++ ConsoleApp4/Entities/Reservation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleApp4.Entities.Exceptions;
+
+namespace ConsoleApp4.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Erro a data de checkOut deve ser maior que a data de  checkIn!!");
+            }
+
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+
+            if (checkIn < now || checkOut < now)
+            {
+                throw new DomainException( "Erro na reserva o checkIn e checkOut para atualização devem ser datas futuras");
+            }
+
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException( "Erro a data de checkOut deve ser maior que a data de  checkIn!!");
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room"
+                +
+                RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + " , "
+                + Duration()
+                + " nights";
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System;
+using System;
+using ConsoleApp4.Entities;
+using ConsoleApp4.Entities.Exceptions;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+
+                Console.WriteLine("Enter data to update the reservation: ");
+
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Erro na reserva: " + e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Erro no formato inserido: " + e.Message);
+            }
+        }
+    }
+}
